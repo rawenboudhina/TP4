34d61b79cb5d72df5391d021f1efcd6822274366
RAWEN <boudhinarawen8@gmail.com>
2023-10-25 22:08:20 +0100

    Mettre à jour les styles de fin de ligne

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlatsComponent } from './plats/plats.component';
+import { AddPlatComponent } from './add-plat/add-plat.component';
+import { UpdatePlatComponent } from './update-plat/update-plat.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [{path: "plats" ,component:PlatsComponent},
+{path: "add-plat",component:AddPlatComponent},
+{path: "updatePlat/:id", component: UpdatePlatComponent},
+{ path: "", redirectTo: "plats", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlatsComponent } from './plats/plats.component';
+import { AddPlatComponent } from './add-plat/add-plat.component';
+import { FormsModule } from '@angular/forms';
+import { UpdatePlatComponent } from './update-plat/update-plat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlatsComponent,
+    AddPlatComponent,
+    UpdatePlatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
