d9c8d09037c37559b11058abec1801ddc9392e0b
RAWEN <boudhinarawen8@gmail.com>
2023-10-25 22:17:04 +0100

    TP4 projet angular

--- src/app/update-plat/update-plat.component.ts
+++ src/app/update-plat/update-plat.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
 import { PlatService } from '../services/plat.service';
 import { Plat } from '../model/plat.model';
+<<<<<<< HEAD
 import { stylecuisine } from '../model/stylecuisine.model';
+=======
+>>>>>>> 982d0a1 (TP4 projet angular)
 
 @Component({
   selector: 'app-update-plat',
@@ -10,13 +13,17 @@ import { stylecuisine } from '../model/stylecuisine.model';
 })
 export class UpdatePlatComponent {
   currentPlat=new Plat();
+<<<<<<< HEAD
   styledecuisines! : stylecuisine[];
   updatedstyleId! : number;
 
+=======
+>>>>>>> 982d0a1 (TP4 projet angular)
   constructor(private activatedRoute: ActivatedRoute,
     private router :Router,
     private platService: PlatService){}
     ngOnInit() {
+<<<<<<< HEAD
       //console.log(this.activatedRoute.snapshot.params['id']);
       this.currentPlat = this.platService.consulterPlat(this.activatedRoute.snapshot. params['id']);
       this.styledecuisines=this.platService.listestylecuisines();
@@ -25,6 +32,14 @@ export class UpdatePlatComponent {
 }
 updatePlat(){
     this.currentPlat.stylecuisine=this.platService.consulterstylecuisine(this.updatedstyleId);
+=======
+      console.log(this.activatedRoute.snapshot.params['id']);
+      this.currentPlat = this.platService.consulterPlat(this.activatedRoute.snapshot. params['id']);
+      console.log(this.currentPlat);
+}
+updatePlat(){
+
+>>>>>>> 982d0a1 (TP4 projet angular)
   //console.log(this.currentPlat);
   this.platService.updatePlat(this.currentPlat);
   this.router.navigate(['plats']);
